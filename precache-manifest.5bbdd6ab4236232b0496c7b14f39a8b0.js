924ae12e8d16db7e2c5dc9e1c72f2b9499841cd3
SadMuMu <289926353@qq.com>
2022-04-01 20:15:47 +0800

    deploy

---
+++ precache-manifest.5bbdd6ab4236232b0496c7b14f39a8b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9d15417b669732aa2de",
+    "url": "/money-vue-website/css/app.bdbbf7ea.css"
+  },
+  {
+    "revision": "c4351a36b635ffd7a71d341a7b331951",
+    "url": "/money-vue-website/index.html"
+  },
+  {
+    "revision": "c9d15417b669732aa2de",
+    "url": "/money-vue-website/js/app.31f6c84e.js"
+  },
+  {
+    "revision": "778f36280a8866a16978",
+    "url": "/money-vue-website/js/chunk-vendors.91fa8007.js"
+  },
+  {
+    "revision": "f2ef55801f65a0060f1763de4116eca1",
+    "url": "/money-vue-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-vue-website/robots.txt"
+  }
+]);
